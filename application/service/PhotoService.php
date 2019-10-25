a9e5f756c56a1f2a357081771b3fc763ade39d0b
hiliqi <hiliqi@gmail.com>
2019-10-26 00:22:08 +0800

    修复章节无图片导致的BUG

--- application/service/PhotoService.php
+++ application/service/PhotoService.php
@@ -36,21 +36,33 @@ class PhotoService
             $data = Photo::where('chapter_id', '=', $chapter_id)->order('pic_order','asc')->select()->toArray();
             cache('pagedPhoto:'.$chapter_id, $data, null, 'redis');
         }
-
-        if ($num <= 0) { //如果分页数设置小于等于0，则默认不分页，也就是全部图片分一页
-            $num = count($data);
+        if (count($data) <= 0) { //如果没有图片
+            return [
+                'photos' => [],
+                'page' => [
+                    'total' => 0,
+                    'last_page' => 0,
+                    'current_page' => 1
+                ]
+            ];
+        } else {
+            if ($num <= 0) { //如果分页数设置小于等于0，则默认不分页，也就是全部图片分一页
+                $num = count($data);
+            }
+            $start = ($page-1) * $num; #计算每次分页的开始位置
+            $total = count($data);
+            $last_page = (int)ceil($total / $num); #计算总页面数
+            $pagedata = array_slice($data, $start, $num);
+            return [
+                'photos' => $pagedata,
+                'page' => [
+                    'total' => $total,
+                    'last_page' => $last_page,
+                    'current_page' => $page
+                ]
+            ];
         }
-        $start = ($page-1) * $num; #计算每次分页的开始位置
-        $total = count($data);
-        $last_page = (int)ceil($total / $num); #计算总页面数
-        $pagedata = array_slice($data, $start, $num);
-        return [
-            'photos' => $pagedata,
-            'page' => [
-                'total' => $total,
-                'last_page' => $last_page,
-                'current_page' => $page
-            ]
-        ];
+
+
     }
 }
